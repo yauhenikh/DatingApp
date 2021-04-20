b1b5cacbd720ab1a893eb0b7eb6d6eeb1ab0ceb7
yauhenikh <khotin.13.nomer@gmail.com>
2021-04-20 12:13:17 +0300

    Add filtering to API

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -32,10 +32,15 @@ namespace API.Data
         public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
             var query = _context.Users
-                            .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                            .AsNoTracking();
+                            .AsQueryable();
 
-            return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
+            query = query.Where(x => x.UserName != userParams.CurrentUsername);
+            query = query.Where(x => x.Gender == userParams.Gender);
+
+            return await PagedList<MemberDto>.CreateAsync(
+                query.ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+                     .AsNoTracking(),
+                userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
