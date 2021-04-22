dcb175792fd9d20be4d782129d6d557204329e8d
yauhenikh <khotin.13.nomer@gmail.com>
2021-04-22 21:05:20 +0300

    Add endpoint for getting messages

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -1,17 +1,22 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
 using API.Helpers;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 
 namespace API.Data
 {
     public class MessageRepository : IMessageRepository
     {
         private readonly DataContext _context;
-        public MessageRepository(DataContext context)
+        private readonly IMapper _mapper;
+        public MessageRepository(DataContext context, IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;
         }
 
@@ -31,9 +36,23 @@ namespace API.Data
                                  .FindAsync(id);
         }
 
-        public Task<PagedList<MessageDto>> GetMessagesForUser()
+        public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
         {
-            throw new System.NotImplementedException();
+            var query = _context.Messages
+                                .OrderByDescending(x => x.MessageSent)
+                                .AsQueryable();
+
+            query = messageParams.Container switch
+            {
+                "Inbox" => query.Where(x => x.Recipient.UserName == messageParams.Username),
+                "Outbox" => query.Where(x => x.Sender.UserName == messageParams.Username),
+                _ => query.Where(x => x.Recipient.UserName == messageParams.Username
+                                      && x.DateRead == null)
+            };
+
+            var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
+
+            return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
         public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
