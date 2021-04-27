efbdfc099064e243a9a9b30a5663a19525434e0d
yauhenikh <khotin.13.nomer@gmail.com>
2021-04-27 20:26:56 +0300

    Add IdentityAdded migration

--- API/Program.cs
+++ API/Program.cs
@@ -20,7 +20,7 @@ namespace API
             {
                 var context = services.GetRequiredService<DataContext>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedUsers(context);
+                // await Seed.SeedUsers(context);
             }
             catch (Exception ex)
             {
