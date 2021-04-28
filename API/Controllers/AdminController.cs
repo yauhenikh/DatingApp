44acd5d223783bd9986e8ef640de6058167d7c76
yauhenikh <khotin.13.nomer@gmail.com>
2021-04-28 12:14:36 +0300

    Add getting users with roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,15 +1,40 @@
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class AdminController : BaseApiController
     {
+        private readonly UserManager<AppUser> _userManager;
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("users-with-roles")]
-        public ActionResult GetUsersWithRoles()
+        public async Task<ActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admins can see this");
+            var users = await _userManager.Users
+                                          .Include(x => x.UserRoles)
+                                          .ThenInclude(x => x.Role)
+                                          .OrderBy(x => x.UserName)
+                                          .Select(x => new
+                                          {
+                                              x.Id,
+                                              Username = x.UserName,
+                                              Roles = x.UserRoles
+                                                       .Select(x => x.Role.Name)
+                                                       .ToList()
+                                          })
+                                          .ToListAsync();
+
+            return Ok(users);
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
